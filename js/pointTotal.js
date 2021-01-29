b67b3d6290941a20bc3c5f368af9588bdc6c9998
Graham Leach-Krouse <gleachkr@gmail.com>
2021-01-29 10:59:15 -0600

    add pointTotal.js for calculating points

---
+++ js/pointTotal.js
@@ -0,0 +1,13 @@
+var exes = Array.from(document.getElementsByClassName("exercise"))
+var points = exes.map(ex => {
+    let customized = Array.from(ex.children).filter(child => child.getAttribute("data-carnap-points"))
+    if (customized.length == 0) return 5
+    else return Number(customized[0].getAttribute("data-carnap-points"))
+}).reduce((a,b) => {return a + b})
+
+try {
+    let pointspans = document.getElementsByClassname("pointspan")
+    Array.from(pointspans).forEach(x => {x.innerHTML = points})
+} catch (e) {
+    console.log("using pointTotal.js requires an element with the class 'pointspan'")
+}
