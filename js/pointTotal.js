9df544ffede5c0ea9cbb30bd9662a4f7f5889073
Graham Leach-Krouse <gleachkr@gmail.com>
2021-02-05 11:49:47 -0600

    Handle some edge cases for pointTotal.js

--- js/pointTotal.js
+++ js/pointTotal.js
@@ -1,6 +1,10 @@
 var exes = Array.from(document.getElementsByClassName("exercise"))
 var points = exes.map(ex => {
-    let customized = Array.from(ex.children).filter(child => child.getAttribute("data-carnap-points"))
+    let kids = Array.from(ex.children)
+    let submissible = kids.filter(child => child.hasAttribute("data-carnap-submission"))
+    if (submissible.length == 0) return 0
+    if (submissible[0].getAttribute("data-carnap-submission").substr(0,6) != "saveAs") return 0
+    let customized = kids.filter(child => child.hasAttribute("data-carnap-points"))
     if (customized.length == 0) return 5
     else return Number(customized[0].getAttribute("data-carnap-points"))
 }).reduce((a,b) => {return a + b})
